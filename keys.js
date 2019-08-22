a47d2a147090e8f4c8251ebe4d5783a8045e69e8
Marino Carranza <marino.carranza@gmail.com>
2019-08-21 18:39:39 -0700

    adds installation of movie and bands packages and some code to go with it

--- keys.js
+++ keys.js
@@ -3,7 +3,19 @@
 exports.spotify = {
 
     // The process.env property returns an object containing the user environment
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.DB_USER,
+    secret: process.env.DB_PASS
 };
 console.log('line 6 --> ', process.env);
+
+exports.bandsintown = {
+    id: process.env.BANDSINTOWN_ID
+}
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+}
+
+// exports.mapquest = {
+//     id: process.env.MAPQUEST_ID
+// }
--- liriBot.js
+++ liriBot.js
@@ -7,7 +7,7 @@
 require('dotenv').config();
 
 // importing the node-spotify-api
-var music = require('node-spotify-api');
+var spotify = require('node-spotify-api');
 
 // importing the api keys
 var keys = require('./keys');
@@ -22,3 +22,71 @@ var moment = require('moment');
 
 // The Node.js file system module allows you to work with the file system on your computer (w3 schools).
 var fs = require('fs');
+
+// initializing the spotyfy api client using my id and secret
+
+var spotify = new spotify(keys.spotify);
+
+// function to get performer
+var getArtistNames = function (artist) {
+    return artist.name;
+};
+
+// searching for music
+var getmeSpotify = function (songName) {
+    if (songName === undefined); {
+        songName = 'defiying gravity';
+    }
+
+    spotify.search({
+        type: 'track',
+        query: songName
+    },
+        function (err, data) {
+            if (error) {
+                console.log('did not work --> ' + err);
+                return;
+            }
+            var songs = data.tracks.item;
+    
+            for (var i = 0; i < songName.length; i++) {
+                console.log(i);
+                console.log('performer --> ' + songs[i].artist.map(getArtistNames));
+                console.log('the song is --> ' + songs[i].name);
+                console.log('preview song --> ' + songs[i].preview_url);
+                console.log('the album is --> ' + songs[i].album);
+            };
+
+        });  
+}
+
+// searching for bands
+var findBand = function (artist) {
+    var queryURL = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=';
+
+    axios.get(queryURL).then(
+        function (response) {
+
+            var jsonData = response.data;
+
+            if (!jasonData.length) {
+                console.log('No results found for --> ' + artist);
+
+                for (var i = 0; i < json.Data.length; i++) {
+                    var show = jasonData[i];
+                
+                    console.log(show.venue.city + ',' + (show.venue.region || show.venue.country) + ' at ' + show.venue.name + ' ' + moment(show.date.time).format('MM/DD/YYYY'));
+                };
+            };
+        }
+    );
+};
+
+// searching for movies
+var findMovie = function (movieName) {
+    if (movieName === undefined) {
+        movieName = 'As good as it gets';
+    }
+}
+
+var urlHit = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=full%tomatoes=true&apikey=trilogy';
