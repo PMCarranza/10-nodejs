982998b7b5d0fef73fb95f9e5d1951e63c5ad3d1
Marino Carranza <marino.carranza@gmail.com>
2019-08-25 20:30:53 -0700

    all functions are running, file needs to have commented out code cleaned up; needs to have a couple of results to movie info

--- liriBot.js
+++ liriBot.js
@@ -58,13 +58,11 @@ var getmeSpotify = function (songName) {
             // for loop to match the artists to the songs found
             for (var j = 0; j < songs[i].artists.length; j++) {
                 // console.log('What does j have--> '[j]);
-                console.log('Performer --> ' + songs[i].artists[j].name);
-                // console logging the value of i at each iteration for each value of the object 
-                // console.log('line 61 song ' + songs[i].artists[j].name);
+                console.log('Performer: ' + songs[i].artists[j].name);
             };
-            console.log('The song is --> ' + songs[i].name);
-            console.log('Preview song --> ' + songs[i].preview_url);
-            console.log('The album is --> ' + songs[i].album.name);
+            console.log('Song: ' + songs[i].name);
+            console.log('Song preview: ' + songs[i].preview_url);
+            console.log('From the album: ' + songs[i].album.name);
             console.log('==========================');
         };
     });
@@ -73,10 +71,6 @@ var getmeSpotify = function (songName) {
 const omdb = new (require('omdbapi'))('68c2d4d3');
 var firstInput = process.argv[2];
 var secondInput = process.argv[3];
-
-// defining place holder variables to be passed values in lines 147 and 148
-var doThis;
-var doThisToo;
 
 var findMovie = function (movieName) {
     if (!movieName) {
@@ -89,22 +83,36 @@ var findMovie = function (movieName) {
         // type: 'series',             // optionnal  ['series', 'episode', 'movie']
         // year: '2010'          // optionnal
         // page: '1' 
-    }).then(res => {
-        console.log(res);
-        console.log('Title: ' + res[1].title);
-        console.log('Release year: ' + res[2].year);
-    }).catch(console.error);
-    // omdb.get({
-    //     id: 'tt0944947',            // optionnal (requires imdbid or title)
-    //     title: 'Game of Thrones',   // optionnal (requires imdbid or title)              // optionnal
-    //     type: 'series',             // optionnal ['series', 'episode', 'movie']
-    //     plot: 'full',               // optionnal (defaults to 'short')
-    //     tomatoes: true,             // optionnal (get rotten tomatoes ratings)
-    //     year: '2011'   
-    // }).then(res => {
-    //     console.log('got response:', res);
-    // }).catch(console.error);
+    }).then(function (res) {
+        // for (var i = 0; i < res.length; i++) {
+            // var jsonData = JSON.stringify(res);
+            // console.log('vvvvv jsonData vvvvvvvvvv');
+            // console.log(jsonData);
+            // console.log('^^^^^^^^  jasonData  ^^^^^^^^^^');
+            // console.log('vvvvv THIS IS RES vvvvvvvvvv');
+            // console.log(res);
+            // console.log('^^^^^^^^  THIS WAS RES  ^^^^^^^^^^^');
+            console.log('Title: ' + res[1].title);
+            console.log('Release year: ' + res[2].year);
+            // console.log('Rating: ' + res[3].imbd.rating);
+        // };
+
+        // }).catch(console.error);
+        // omdb.get({
+        //     id: 'movie'            // optionnal (requires imdbid or title)
+        // // //     title: 'Game of Thrones',   // optionnal (requires imdbid or title)              // optionnal
+        // // //     type: 'series',             // optionnal ['series', 'episode', 'movie']
+        // //     plot: 'short',               // optionnal (defaults to 'short')
+        // //     tomatoes: true,             // optionnal (get rotten tomatoes ratings)
+        // //     year: '2011'   
+        // }).then(res => {
+        //     // console.log('Plot--> ' + plot);
+        //     console.log('tomatoes --> ' + tomatoes);
+        //     console.log(res);
+        // }).catch(console.error);
+});
 };
+
 
 // searching for bands
 var findBand = function (artist) {
@@ -114,7 +122,7 @@ var findBand = function (artist) {
     //     artist = 'the beach boys';
     //     // console.log('artist is --> ' + artist);
     // }
-    
+
     console.log('hora --> ' + moment().format('h:mm:ss a'));
     console.log('artist ' + artist);
     axios.get(queryURL).then(
@@ -122,29 +130,23 @@ var findBand = function (artist) {
             // console.log('vvvvv  THIS IS RESPONES  vvvvv');
             // console.log(response);
             // console.log('^^^^^^^^^  THIS WAS RESPONSE  ^^^^^^^^^^^^');
-         
-            // console.log(response);
-            var performer = response.data;
+
+            var secondInput = response.data;
             // console.log('vvvvvvvvv  this is response.data vvvvvvvvvv');
-            // console.log(performer);
+            // console.log(secondInput);
             // console.log('^^^^^^^^^^^this was response.data^^^^^^^^^ ');
 
-            // console.log('firstInput has --> ' + firstInput);
-            // console.log('secondInput has --> ' + secondInput);
-            // if (!performer.length) {
-                for (var i = 0; i < performer.length; i++) {
-                    var show = performer[i];
-                    // console.log('Show  vvvvvvv');
-                    // console.log(show);
-                    // console.log('Show ^^^^^^^^^^^^');
-                    console.log('========== INFORMATION I AM LOOKIING FOR =================');
-                    console.log('Performer: ' + show.lineup[0]);
-                    console.log('Venue: ' + show.venue.name);
-                    console.log('City: ' + show.venue.city + ', ' + show.venue.region);
-                    console.log('Date & Time of Show: ' + moment(show.datetime).format('MM/DD/YYYY hh:00 A'));
-                    console.log('=========================================================');
-                    console.log('');
-                };
+            // if (!secondInput.length) {
+            for (var i = 0; i < secondInput.length; i++) {
+                var show = secondInput[i];
+                console.log('========== INFORMATION I AM LOOKIING FOR =================');
+                console.log('Performer: ' + show.lineup[0]);
+                console.log('Venue: ' + show.venue.name);
+                console.log('City: ' + show.venue.city + ', ' + show.venue.region);
+                console.log('Date & Time of Show: ' + moment(show.datetime).format('MM/DD/YYYY hh:00 A'));
+                console.log('=========================================================');
+                console.log('');
+            };
             // };
         }
     );
@@ -159,16 +161,16 @@ var doWhatItSays = function () {
         }
         // dataArr gets what random.txt has and uses the .split() method to separate the objects in the new array
         var dataArr = data.split(",");
-        console.log('data contains this on line 139 --> ' + data);
+        // console.log('data contains this --> ' + data);
 
         // after split() doThis gets 'spotify-this-song' and and dothisToo gets 'God I hate Shakespeare'
-        console.log('dataArr at 0 has --> ' + dataArr[0]);
-        console.log('dataArr at 1 has --> ' + dataArr[1]);
-        doThis = dataArr[0];
-        doThisToo = dataArr[1];
+        // console.log('dataArr at 0 has --> ' + dataArr[0]);
+        // console.log('dataArr at 1 has --> ' + dataArr[1]);
+        firstInput = dataArr[0];
+        secondInput = dataArr[1];
 
         // run the function with its parameters
-        whichFunction(doThis, doThisToo);
+        whichFunction(firstInput, secondInput);
     });
 };
 
